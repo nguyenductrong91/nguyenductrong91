a58030ee23fe151ab8752d4142b540e5e177acf8
nguyenductrong <nguyenductrong16191@gmail.com>
2021-10-14 18:14:31 +0700

    bai tap so 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,32 @@
 package com.company;
 
 public class Main {
+        public static void voidBMI(double weight,double height) {
+            System.out.println("chi so BMI kieu void la:"+ weight/(height*height));
+        }
+        public static double doubleBMI (double weight,double height) {
+            System.out.println("chi so BMI kieu double la:"+ weight/(height*height));
+            return weight/(height*height);
+        }
+        public static double noneParamBMI () {
+            double weight  ;
+            double height;
+            weight =60;
+            height = 1.65;
+            System.out.println("chi so BMI kieu noneparamBMI la:"+ weight/(height*height));
+            return weight/(height*height);
+        }
 
-    public static void main(String [] args) throws Exception {
-        double height =1.65;
-        double weight = 60;
-        double BMI;
-        BMI = weight / ( height * height);
-        System.out.println("Your BMI is:"+ BMI);
+        public static void main(String [] args) throws Exception {
+           voidBMI(60,1.65);
+           doubleBMI(60,1.65);
+           noneParamBMI();
+
+
+
+
     }
+
 
 }
 
